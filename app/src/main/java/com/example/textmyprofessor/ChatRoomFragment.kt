074b79025328fbde0d5b154e0c4043062bcd0761
Spencer Young <spenceryoungcs@gmail.com>
2019-11-18 11:23:18 -0800

    Chatroom fragment now navigates

--- app/src/main/java/com/example/textmyprofessor/ChatRoomFragment.kt
+++ app/src/main/java/com/example/textmyprofessor/ChatRoomFragment.kt
@@ -8,28 +8,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import com.example.textmyprofessor.databinding.FragmentCreateRoomBinding
+import com.example.textmyprofessor.databinding.FragmentChatRoomBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class CreateRoomFragment : Fragment() {
-
+class ChatRoomFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentCreateRoomBinding>(inflater,
-            R.layout.fragment_create_room,container,false)
         // Inflate the layout for this fragment
 
-        binding.createRoomBtn.setOnClickListener{view: View ->
-            view.findNavController().navigate(R.id.action_createRoomFragment_to_ChatRoomFragment)
-        }
-//        return inflater.inflate(R.layout.fragment_create_room, container, false)
+        val binding = DataBindingUtil.inflate<FragmentChatRoomBinding>(inflater,
+            R.layout.fragment_chat_room,container,false)
+
         return binding.root
     }
 
