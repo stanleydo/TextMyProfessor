e250dcf2e6c5021e15b15f4d9d571e28ac986fe7
BuildTools <unconfigured@null.spigotmc.org>
2019-11-21 18:23:43 -0800

    Implemented firebase auth/database Added RecyclerView to ChatRoom (Chat Box) ## TODO Added Room creation to firebase realtime database Added Professor chat to same room in database

--- app/src/main/java/com/example/textmyprofessor/ChatRoomFragment.kt
+++ app/src/main/java/com/example/textmyprofessor/ChatRoomFragment.kt
@@ -1,14 +1,20 @@
 package com.example.textmyprofessor
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.textmyprofessor.databinding.FragmentChatRoomBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +22,9 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class ChatRoomFragment : Fragment() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +33,16 @@ class ChatRoomFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentChatRoomBinding>(inflater,
             R.layout.fragment_chat_room,container,false)
+
+        database = FirebaseDatabase.getInstance().reference
+
+        val args = ChatRoomFragmentArgs.fromBundle(arguments!!)
+        val room_id = args.roomid
+
+        binding.sendBtn.setOnClickListener{
+            val text = binding.inputMsgText.text.toString()
+            database.child("chat-rooms").child(room_id).child("Professor at " + Date()).setValue(text)
+        }
 
         return binding.root
     }
--- app/src/main/java/com/example/textmyprofessor/CreateRoomFragment.kt
+++ app/src/main/java/com/example/textmyprofessor/CreateRoomFragment.kt
@@ -10,6 +10,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.textmyprofessor.databinding.FragmentCreateRoomBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_create_room.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +21,9 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class CreateRoomFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +33,17 @@ class CreateRoomFragment : Fragment() {
             R.layout.fragment_create_room,container,false)
         // Inflate the layout for this fragment
 
-        binding.createRoomBtn.setOnClickListener{view: View ->
-            view.findNavController().navigate(R.id.action_createRoomFragment_to_ChatRoomFragment)
+        database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+
+        auth.signInAnonymously()
+
+        binding.createRoomBtn.setOnClickListener{
+                view: View -> view.findNavController().navigate(CreateRoomFragmentDirections.actionCreateRoomFragmentToChatRoomFragment(binding.roomID.text.toString()))
+            database.child("chat-rooms").child(binding.roomID.text.toString()).setValue("")
         }
+
+
 //        return inflater.inflate(R.layout.fragment_create_room, container, false)
         return binding.root
     }
